a831df6c9542069f5cb3f7dfd641f881cf77b959
moi <125820548+nojia1775@users.noreply.github.com>
2024-11-25 13:38:39 +0000

    modification du main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,10 +3,12 @@
 int	main(void)
 {
 	Zombie	*eva00;
+	Zombie eva02("Eva02");
 
 	eva00 = newZombie("Eva00");
 	randomChump("Eva01");
 	eva00->announce();
+	eva02.announce();
 	delete eva00;
 	return (0);
 }
